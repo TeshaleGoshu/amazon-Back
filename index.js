de5c6d87d57b3cc5ce03fbc6d4a75c31dd1060c8
Teshale <teshalegoshu1@gmail.com>
2024-01-18 04:12:01 +0000

    stripe corrected

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Stripe from "stripe";
+import Stripe from '@stripe/react-stripe-js';
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
